56699ca35c861cf384f8fcc14545e88953b2c0ee
PingJiaJia <2867713502@qq.com>
2020-08-19 23:00:12 +0800

    2.修改util.Date&sql.Date

--- src/person/TestTimes.java
+++ src/person/TestTimes.java
@@ -9,14 +9,20 @@ public class TestTimes {
         System.out.println(new java.util.Date());
 
         String str="1997-09-09";
+        //将字符串转为Date
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
         try {
-            java.util.Date data=sdf.parse(str);    //将字符串转为Date
-            System.out.println(data);
+            //parse方法转换为util.Date
+            java.util.Date date=sdf.parse(str);
+            System.out.println(date);
             System.out.println("-----------");
+            //format方法转换为String类型
             String dates=sdf.format(new java.util.Date());
             System.out.println(dates);
 
+            //sql.Date不支持字符串转换   只支持毫秒创建
+            //通过util.Date拿到指定日期的毫秒值，转换为sql.Date
+            java.sql.Date sqlDate=new java.sql.Date(date.getTime());
 
         } catch (ParseException e) {
             e.printStackTrace();
