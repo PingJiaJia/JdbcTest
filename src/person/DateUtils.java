44ab46d14b5765dedce0fc7465ef3a62cd3204d5
PingJiaJia <2867713502@qq.com>
2020-08-20 08:10:06 +0800

    插入的日期修改结束

--- src/person/DateUtils.java
+++ src/person/DateUtils.java
@@ -8,7 +8,7 @@ public class DateUtils {
 
     private static SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
     //1.字符串转换为util.Date
-    public java.util.Date utilToStr(String str){
+    public static java.util.Date strToUtil(String str){
         try {
             java.util.Date date = sdf.parse(str);
             return date;
@@ -19,12 +19,12 @@ public class DateUtils {
     }
 
     //2.将util.Date转化为sql.Date
-    public java.sql.Date utilToSql(java.util.Date date){
+    public static java.sql.Date utilToSql(java.util.Date date){
         return new java.sql.Date(date.getTime());
     }
 
     //3.util.Date转化为字符串
-    public String utilToStr(java.util.Date date){
+    public static String utilToStr(java.util.Date date){
         return sdf.format(date);
     }
 
--- src/person/TestPerson.java
+++ src/person/TestPerson.java
@@ -3,7 +3,7 @@ package person;
 public class TestPerson {
     public static void main(String[] args) {
         PersonDaoImpl personDao = new PersonDaoImpl();
-        Person person = new Person("胡某灿",23,null,"pingjiacheng@rongzi.com","江苏省南通市");
+        Person person = new Person("Marry",23,DateUtils.strToUtil("2000-06-02"),"Marry@qq.com","江苏省南通市");
         int result=personDao.insert(person);
         if(result==1){
             System.out.println("插入成功");
