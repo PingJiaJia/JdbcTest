4cddf223dc1fc96d6e8a7af6b27671bf07971fc3
PingJiaJia <2867713502@qq.com>
2020-08-20 22:36:24 +0800

    应用DateUtils完善DAO

--- src/person/TestPersonUpdate.java
+++ src/person/TestPersonUpdate.java
@@ -3,7 +3,7 @@ package person;
 public class TestPersonUpdate {
     public static void main(String[] args) {
         PersonDaoImpl personDao=new PersonDaoImpl();
-        Person person=new Person(3,"胡金灿",19,null,"hjc@qq.com","湖北省武汉市");
+        Person person=new Person(2,"胡金灿",19, DateUtils.strToUtil("2020-01-30"),"hujc@qq.com","湖北省武汉市");
         int result = personDao.update(person);
         if(result==1){
             System.out.println("修改成功！");
