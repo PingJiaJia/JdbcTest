9f3aa02101ed261b528613c13d74ea0d9fc4a9a6
PingJiaJia <2867713502@qq.com>
2020-08-22 23:15:44 +0800

    Account转账案例DAO搭建

---
+++ src/account/DBUtils.java
@@ -0,0 +1,54 @@
+package account;
+
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class DBUtils {
+     private static final Properties PROPERTIES=new Properties();
+
+     static {
+         InputStream inputStream=DBUtils.class.getResourceAsStream("/db.properties");
+         try {
+             PROPERTIES.load(inputStream);
+             Class.forName(PROPERTIES.getProperty("driver"));
+         } catch (IOException e) {
+             e.printStackTrace();
+         } catch (ClassNotFoundException e) {
+             e.printStackTrace();
+         }
+     }
+
+     public static Connection getConnection(){
+         try {
+             Connection connection= DriverManager.getConnection(PROPERTIES.getProperty("url"),PROPERTIES.getProperty("user"),PROPERTIES.getProperty("password"));
+             return null;
+         } catch (SQLException throwables) {
+             throwables.printStackTrace();
+         }
+         return null;
+     }
+
+     public static void closeAll(Connection connection, Statement statement, ResultSet resultSet){
+         try {
+             if(resultSet!=null){
+                 resultSet.close();
+             }
+             if (statement!=null){
+                 statement.close();
+             }
+             if (connection!=null){
+                 connection.close();
+             }
+         } catch (SQLException throwables) {
+             throwables.printStackTrace();
+         }
+
+
+     }
+
+
+}
---
+++ src/account/accountDao.java
@@ -0,0 +1,59 @@
+package account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class accountDao {
+    public int insert(Account account){ return 0;}
+    public int delete(String cardNo){ return 0;}
+    public int update(Account account){
+        Connection connection=null;
+        PreparedStatement preparedStatement=null;
+        String sql="update account set password=?,name=?,balance=? where cardNo=?";
+
+        connection=DBUtils.getConnection();
+        try {
+            preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,account.getPassword());
+            preparedStatement.setString(2,account.getName());
+            preparedStatement.setDouble(3,account.getBalance());
+            int result=preparedStatement.executeUpdate();
+            return result;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }finally {
+            DBUtils.closeAll(connection,preparedStatement,null);
+        }
+
+        return 0;
+    }
+
+    public Account select(String cardNo){   //需要再看一下
+        Connection connection=null;
+        PreparedStatement preparedStatement=null;
+        String sql="select * from account where cardNo=?";
+        Account account=null;
+        connection=DBUtils.getConnection();
+        try {
+            preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setString(1,cardNo);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                String cardNos=resultSet.getString("cardNo");
+                String password=resultSet.getString("password");
+                String name=resultSet.getString("name");
+                Double balance=resultSet.getDouble("balance");
+                account=new Account(cardNos,password,name,balance);
+            }
+            return account;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+}
